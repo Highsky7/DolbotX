3d4ff358f0dbf89a7ac4915348a0420371337295
Highsky7 <albert31115@gmail.com>
2025-09-08 15:39:13 +0900

    feat: Add entry points with onnx_multi_traffic_supply

--- src/robot_vision/setup.py
+++ src/robot_vision/setup.py
@@ -27,7 +27,7 @@ setup(
             'onnx_traffic = robot_vision.onnx_traffic:main',
             'onnx_multi_traffic = robot_vision.onnx_multi_traffic:main',
             'onnx_traffic_qos = robot_vision.onnx_traffic_qos:main',
-            'onnx_multi_traffic_qos = robot_vision.onnx_multi_traffic_qos:main',
+            'onnx_multi_traffic_supply = robot_vision.onnx_multi_traffic_supply:main',
             'nobev_path_planner_pp = robot_vision.nobev_path_planning_pp:main',
             'yolo_traffic_optimized = robot_vision.yolo_traffic_optimized:main',
             'yolo_traffic_qos_optimized = robot_vision.yolo_traffic_qos_optimized:main',
